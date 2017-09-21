5278ed59e34ee7c56b709992e78c3c3389382e44
kurogosane1 <magnite@hotmail.com>
2017-09-20 21:30:16 -0500

    got to play and pause

--- music/src/app/app.component.ts
+++ music/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ListAddService } from './list-add.service';
+import {Howl} from 'howler';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +8,25 @@ import { ListAddService } from './list-add.service';
 })
 export class AppComponent {
   title = 'app';
+  sound: any;
 
-  constructor(private musicService: ListAddService) {}
+  constructor() {
+    this.sound = new Howl({
+      src: ['../assets/1-01 Galaxy Supernova.mp3'],
+      onend: function() {
+          console.log('Finished!');
+      },
+      onloaderror: function() {
+          console.log('Error!');
+      },
+  });
+  }
+
+    playMusic() {
+      this.sound.play();
+    }
+
+    pauseMusic() {
+      this.sound.pause();
+    }
 }
--- music/src/app/list-add.service.ts
+++ music/src/app/list-add.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Howl} from 'howler';
 
 @Injectable()
 export class ListAddService {
@@ -6,10 +7,16 @@ export class ListAddService {
   constructor() { }
 
   // to play music
-  playsound() {}
+  playsound() {
+    const sound: any = new Howl({
+      src: ['../assets/1-01 Galaxy Supernova.mp3']
+    });
+  }
 
   // to pause music
-  stopMusic() {}
+  stopMusic() {
+    
+  }
 
   // next music
   nextMusic() {}
